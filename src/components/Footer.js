466874df0423d12143a0c169566ee1c31c85a8e5
Filip Jerga <jerga99@gmail.com>
2021-04-15 15:01:37 +0100

    Removes env production from gitignore

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -26,7 +26,7 @@ export default function Footer(props) {
             <a
               rel="noreferrer"
               target="_blank"
-              href={`https://feedspot.com/infiniterss.php?q=site:${process.env.BASE_URL}/rss.xml`}>
+              href={`https://www.feedspot.com/infiniterss.php?q=site:${process.env.BASE_URL}/rss.xml`}>
               Follow RSS
             </a>
           </div>
