59e6c5dd6b2bca260b8a937c887465b48a3a0939
Filip Jerga <jerga99@gmail.com>
2021-04-16 11:11:41 +0100

    Font awesome icon

--- src/components/RooLayout.js
+++ src/components/RooLayout.js
@@ -1,7 +1,17 @@
 import React from "react"
+import { library, config } from "@fortawesome/fontawesome-svg-core"
+import {
+  faSun,
+  faMoon
+} from "@fortawesome/free-solid-svg-icons"
+
+import "@fortawesome/fontawesome-svg-core/styles.css"
 import "../styles/global.scss"
 import "prismjs/themes/prism-solarizedlight.css"
 import "react-toggle/style.css"
+
+config.autoAddCss = false
+library.add(faSun, faMoon)
 
 export default function RootLayout({children}) {
 
--- src/components/ThemeToggle.js
+++ src/components/ThemeToggle.js
@@ -2,9 +2,16 @@
 
 import React from "react"
 import Toggle from "react-toggle"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 export default function ThemeToggle({className}) {
   return (
-    <Toggle className={`day-night-toggle ${className}`}/>
+    <Toggle
+      className={`day-night-toggle ${className}`}
+      icons={{
+        checked: <FontAwesomeIcon inverse icon="sun" />,
+        unchecked: <FontAwesomeIcon inverse icon="moon" />
+      }}
+    />
   )
 }
